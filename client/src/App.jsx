075206934dbaccff34bc05544744c8142f7eabbe
Varun Chillara <varunkamesh22@gmail.com>
2021-10-26 19:56:07 -0400

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react";
+
+function App (props) {
+
+  return (
+    <>
+      hello world1
+    </>
+    );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json());
+app.use(express.static(__dirname + "/../client/dist"));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
